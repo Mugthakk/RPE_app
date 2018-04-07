5da7cdcb64b9b773ff644a6e3b6403182c352af9
Sander <hanakmumakil23@gmail.com>
2018-04-07 12:14:05 +0200

    Removes padding from scrollview to let it scroll again and fixes modal dismiss typo so both modals now dismiss from touchablewithoutfeedback's onpress

--- RPE_app/components/rpeInfo.js
+++ RPE_app/components/rpeInfo.js
@@ -11,39 +11,38 @@ export default class RPEInfo extends Component {
 
   render(){
     return(
-    <ScrollView style={{backgroundColor: "grey", paddingHorizontal: 10, paddingTop: 30, paddingBottom: 50}}>
+        <ScrollView style={{backgroundColor: "grey", paddingHorizontal: 10}}>
+          
+          <Button title="RPE to %" color="red" onPress={() => {this.setState({"rpeChartVisible": true})}}/>
 
-      <Button marginHorizontal={20} title="RPE to %" color="red" onPress={() => {this.setState({"rpeChartVisible": true})}}/>
+          <Text style={{color: "white", fontSize: 20}}>{"RPE (rate of perceived exertion) is a rating system that allows athletes to measure how hard something feels to you at the time. It is a subjective measure of your strength at a given time. We rate this on a scale from one to ten. The higher the number, the harder the set felt. It is also a way to quantify those feelings we have immediately post-set of gauging how difficult it was. “I could maybe have done 1 or 2 more reps.” The RPE scale quantifies this."}</Text>
+          
+          <Button title="RPE estimation chart" color = "red" onPress={() => {this.setState({"rpeEstimationVisible": true})}}/>
 
-      <Text style={{color: "white", fontSize: 20}}>{"RPE (rate of perceived exertion) is a rating system that allows athletes to measure how hard something feels to you at the time. It is a subjective measure of your strength at a given time. We rate this on a scale from one to ten. The higher the number, the harder the set felt. It is also a way to quantify those feelings we have immediately post-set of gauging how difficult it was. “I could maybe have done 1 or 2 more reps.” The RPE scale quantifies this."}</Text>
-      
-      <Button marginHorizontal={20} title="RPE estimation chart" color = "red" onPress={() => {this.setState({"rpeEstimationVisible": true})}}/>
+          <Text style={{color: "white", fontSize: 20}}>{"RPE allows you to regulate your training intensity based on your condition right now. Not your last meet, yesterday, or even your last set. It allows you to quantify where your preparedness is at any given time."}</Text>
+          
+          <Modal
+          visible={this.state.rpeChartVisible}
+          onRequestClose={ () => {this.setState ( {"rpeChartVisible": false} )} }>
+            <TouchableWithoutFeedback onPress={() => {this.setState ( {"rpeChartVisible": false} )} }>
+              <Image 
+              style={{flex:1, height: undefined, width: undefined}} 
+              source={require("../images/rpeChart.png")}
+              resizeMode="contain"/>
+            </TouchableWithoutFeedback>
+          </Modal>
 
-      <Text style={{color: "white", fontSize: 20}}>{"RPE allows you to regulate your training intensity based on your condition right now. Not your last meet, yesterday, or even your last set. It allows you to quantify where your preparedness is at any given time."}</Text>
-      
-      <Modal
-      visible={this.state.rpeChartVisible}
-      onRequestClose={ () => {this.setState ( {"rpeChartVisible": false} )} }>
-        <TouchableWithoutFeedback onPress={() => {this.setState ( {"rpeChartVisible": false} )} }>
-          <Image 
-          style={{flex:1, height: undefined, width: undefined}} 
-          source={require("../images/rpeChart.png")}
-          resizeMode="contain"/>
-        </TouchableWithoutFeedback>
-      </Modal>
+          <Modal
+          visible={this.state.rpeEstimationVisible}
+          onRequestClose={ () => {this.setState ( {"rpeEstimationVisible": false} )} }>
+            <TouchableWithoutFeedback onPress={() => {this.setState ( {"rpeEstimationVisible": false} )} }>
+              <Image style={{flex:1, height: undefined, width: undefined}}
+              source={require("../images/rpeEstimation.png")}
+              resizeMode="contain"/>
+            </TouchableWithoutFeedback>
+          </Modal>
 
-      <Modal
-      visible={this.state.rpeEstimationVisible}
-      on
-      onRequestClose={ () => {this.setState ( {"rpeEstimationVisible": false} )} }>
-        <TouchableWithoutFeedback onPress={() => {this.setState ( {"rpe>EstimationVisible": false} )} }>
-          <Image style={{flex:1, height: undefined, width: undefined}}
-          source={require("../images/rpeEstimation.png")}
-          resizeMode="contain"/>
-        </TouchableWithoutFeedback>
-      </Modal>
-
-    </ScrollView>
+        </ScrollView>
     );
   }
 
